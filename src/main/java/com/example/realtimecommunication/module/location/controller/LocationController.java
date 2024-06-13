6070d11ab50e1b9b46ea5bf481449fb6ecfac5cd
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-06-13 13:56:27 +0900

    [FEAT] 위치 공유를 위한 기본 Logic 추가

--- src/main/java/com/example/realtimecommunication/module/location/controller/LocationController.java
+++ src/main/java/com/example/realtimecommunication/module/location/controller/LocationController.java
@@ -1,6 +1,9 @@
 package com.example.realtimecommunication.module.location.controller;
 
+import com.example.realtimecommunication.module.location.dto.LocationDto;
 import com.example.realtimecommunication.module.location.service.LocationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,10 @@ public class LocationController {
 
     public LocationController(LocationService locationService) {
         this.locationService = locationService;
+    }
+
+    @GetMapping("/cur")
+    public ResponseEntity<LocationDto> shareCurLocation() {
+        return ResponseEntity.ok(locationService.shareCurLocation());
     }
 }
--- src/main/java/com/example/realtimecommunication/module/location/service/LocationService.java
+++ src/main/java/com/example/realtimecommunication/module/location/service/LocationService.java
@@ -1,7 +1,18 @@
 package com.example.realtimecommunication.module.location.service;
 
+import com.example.realtimecommunication.module.location.dto.LocationDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LocationService {
+    public LocationDto shareCurLocation() {
+        return makeRandomLocation();
+    }
+
+    private LocationDto makeRandomLocation() {
+        double landomX = Math.random() * 100;
+        double landomY = Math.random() * 100;
+
+        return new LocationDto(landomX,landomY);
+    }
 }
