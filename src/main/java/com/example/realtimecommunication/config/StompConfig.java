85519c570a757a773e70a6c7373149d0b7a93f2d
김승진 <ohksj77@gmail.com>
2024-06-13 14:50:05 +0900

    [FEAT] STOMP 위치 공유 Logic 추가

---
+++ src/main/java/com/example/realtimecommunication/config/StompConfig.java
@@ -0,0 +1,26 @@
+package com.example.realtimecommunication.config;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+public class StompConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(final StompEndpointRegistry registry) {
+        registry.addEndpoint("/location").setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureMessageBroker(final MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");
+    }
+}
---
+++ src/main/java/com/example/realtimecommunication/module/location/controller/StompController.java
@@ -0,0 +1,27 @@
+package com.example.realtimecommunication.module.location.controller;
+
+import com.example.realtimecommunication.module.location.service.LocationService;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StompController {
+
+    private final SimpMessagingTemplate template;
+    private final LocationService locationService;
+
+    public StompController(
+            final SimpMessagingTemplate template, final LocationService locationService) {
+        this.template = template;
+        this.locationService = locationService;
+    }
+
+    @MessageMapping("/share/{id}")
+    public void shareCurLocation(@DestinationVariable final Long id) {
+        template.convertAndSend(
+                String.format("/sub/location/%d", id), locationService.shareCurLocation());
+    }
+}
