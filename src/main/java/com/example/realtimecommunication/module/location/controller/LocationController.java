cf4fa079abdca00240389c389c64246c0c40d0fc
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-06-13 13:57:30 +0900

    [FEAT] 위치 공유를 위한 기본 Logic 추가

--- src/main/java/com/example/realtimecommunication/module/location/controller/LocationController.java
+++ src/main/java/com/example/realtimecommunication/module/location/controller/LocationController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/location")
 public class LocationController {
     private final LocationService locationService;
 
